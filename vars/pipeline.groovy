08e74157aa774324654c6b06a65691c5e61808a8
EC2 Default User <ec2-user@ip-172-31-70-70.ec2.internal>
2023-12-16 09:53:00 +0000

    Added

---
+++ vars/pipeline.groovy
@@ -0,0 +1,12 @@
+def call(){
+    pipeline(){
+        agent any
+        stages{
+            stage("Hello"){
+                steps{
+                    echo 'Hello World'
+                }
+            }
+        }
+    }
+}
