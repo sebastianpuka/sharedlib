b7e4aa9ce7fb033a705aad770ac4fa75823c239b
sebastianpuka <38990928+sebastianpuka@users.noreply.github.com>
2023-12-17 19:57:40 +0100

    Update stdPipeline.groovy

--- vars/stdPipeline.groovy
+++ vars/stdPipeline.groovy
@@ -10,7 +10,7 @@ def call(body) {
         stages{
             stage("Hello"){
                 steps{
-                    echo "Hello"
+                    echo "Hello ${config.name}"
                 }
             }
         }
