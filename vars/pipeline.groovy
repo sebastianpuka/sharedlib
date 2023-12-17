3bc840a260d477d34a8d442cbdf8db55459c8bc7
sebastianpuka <38990928+sebastianpuka@users.noreply.github.com>
2023-12-17 19:09:26 +0100

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -4,7 +4,7 @@ def call(Map config=[:]){
         stages{
             stage("Hello"){
                 steps{
-                    echo "${config.name}"
+                    echo "Hello"
                 }
             }
         }
